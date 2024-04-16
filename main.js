4a4696cb5167aeefe089a92856dcca6c6bd4cda2
Alex0Antone <145033566+Alex0Antone@users.noreply.github.com>
2024-04-16 15:24:36 +0000

    ????

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var a = ["download (1).jpg", "download.jpg", "download (2).jpg", "download (3).j
 var num = 0
 
 NextButton.addEventListener("click", function() {
+    
     num+=1
     num= num%a.length
     image1.src = a[num]
